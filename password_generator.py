71e97fdc7770e79492ff7b6d4cebf952ec71a118
Ashu <ashutoshkhade2530@gamil.com>
2023-12-18 12:17:20 +0530

    added project changes

--- password_generator.py
+++ password_generator.py
@@ -9,14 +9,24 @@ numbers=['0','1','2','3','4','5','6','7','8','9',]
 print("Welcome to the Password Generator \n")
 
 n_symbols=int(input("Enter many symbols you want to in your password \n"))
-n_small_letters=int(input("How many small letters you want in your password \n"))
 n_capital_letters=int(input("How many capital letters you want in your password \n"))
+n_small_letters=int(input("How many small letters you want in your password \n"))
 n_numbers=int(input("How many numbers you want in your password \n"))
+
 password=""
 for result in range(1,n_symbols+1):
-    sym=random.choice(symbols)
-    password=password +sym    
-print(password) 
-for rsult in range(1,n_small_letters+1):
-    small=random.choice 
-   
+    strong_password=random.choice(symbols)
+    password= password + strong_password   
+    
+for result in range(1,n_capital_letters+1):
+    strong_password=random.choice(capital_letters)
+    password +=strong_password
+    
+for result in range(1,n_small_letters+1):
+    strong_password=random.choice(small_letters)    
+    password +=strong_password
+    
+for result in range(1,n_numbers+1)    :
+    strong_password=random.choice(numbers)
+    password +=strong_password
+print(password)   
